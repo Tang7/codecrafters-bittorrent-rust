528db60f99b7404c672b2f1d90218316d23f5cdd
Tang7 <still.tangqi@gmail.com>
2023-11-15 23:05:19 +0200

    Enable debug log

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,6 @@ fn main() {
         let decoded_value = decode_bencoded_value(encoded_value);
         println!("{}", decoded_value.to_string());
     } else {
-        println!("unknown command: {}", args[1])
+        eprintln!("unknown command: {}", args[1])
     }
 }
