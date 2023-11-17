8991d64f95946cfbb2fb6e4a43c77473c13375c5
Tang7 <still.tangqi@gmail.com>
2023-11-17 23:13:09 +0200

    Decode list

--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,42 @@ use std::env;
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    if encoded_value.chars().last().unwrap() != 'e' {
+        panic!("bencode {} should ends with 'e'", encoded_value);
+    }
+
     match encoded_value.chars().next() {
         Some('0'..='9') => {
             if let Some((len, rest)) = encoded_value.split_once(':') {
                 if let Ok(len) = len.parse::<usize>() {
-                    return rest[..len].to_string().into();
+                    return (rest[..len].to_string().into(), &rest[len..]);
                 }
             }
         }
         Some('i') => {
-            if let Some(n) = encoded_value
-                .split_at(1)
-                .1
-                .split_once('e')
-                .and_then(|(digits, _)| {
-                    let n = digits.parse::<i64>().ok()?;
-                    Some(n)
-                })
+            if let Some((n, rest)) =
+                encoded_value
+                    .split_at(1)
+                    .1
+                    .split_once('e')
+                    .and_then(|(digits, rest)| {
+                        let n = digits.parse::<i64>().ok()?;
+                        Some((n, rest))
+                    })
             {
-                return n.into();
+                return (n.into(), rest);
             }
+        }
+        Some('l') => {
+            let mut values = Vec::new();
+            let mut rest = encoded_value.split_at(1).1;
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (v, remainder) = decode_bencoded_value(rest);
+                values.push(v);
+                rest = remainder;
+            }
+            return (values.into(), &rest[1..]);
         }
         _ => {}
     }
@@ -41,7 +56,7 @@ fn main() {
     if command == "decode" {
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("{}", decoded_value.0.to_string());
     } else {
         eprintln!("unknown command: {}", args[1])
     }
